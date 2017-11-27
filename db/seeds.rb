ed9db9ff1c12b2fd90653104e367c1781dc133e5
fijimoto0895 <fujimotosiro@gmail.com>
2017-11-27 22:47:28 +0900

    DESTROY completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.find_or_create_by(id: 1) do |user|
-  user.email = 'ddd@ddd.com'
-  user.password = 'dddddd'
-end
